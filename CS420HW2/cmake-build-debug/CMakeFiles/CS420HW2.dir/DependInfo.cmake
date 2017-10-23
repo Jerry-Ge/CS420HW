c533421efe4dfd55762ba54b38543d2e129fdada
Yuzhou Ge <yuzhouge@usc.edu>
2017-10-22 23:26:22 -0700

    hw#2 first commit

---
+++ CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/adaptcm.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/adaptcm.c.o"
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/example.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/example.c.o"
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/jpeg.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/jpeg.c.o"
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/pic.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/pic.c.o"
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/ppm.1.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/ppm.1.c.o"
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/ppm.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/ppm.c.o"
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/tiff.1.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/tiff.1.c.o"
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/tiff.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/tiff.c.o"
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/xpic.1.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/xpic.1.c.o"
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/xpic.2.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/xpic.2.c.o"
+  "/Users/yuzhouge/Desktop/CS420HW2/pic/xpic.c" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/pic/xpic.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yuzhouge/Desktop/CS420HW2/assign2/assign2.cpp" "/Users/yuzhouge/Desktop/CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/assign2/assign2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CS420HW2/cmake-build-debug/CMakeFiles/CS420HW2.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS420HW2.dir/assign2/assign2.cpp.o"
+  "CMakeFiles/CS420HW2.dir/pic/adaptcm.c.o"
+  "CMakeFiles/CS420HW2.dir/pic/example.c.o"
+  "CMakeFiles/CS420HW2.dir/pic/jpeg.c.o"
+  "CMakeFiles/CS420HW2.dir/pic/pic.c.o"
+  "CMakeFiles/CS420HW2.dir/pic/ppm.1.c.o"
+  "CMakeFiles/CS420HW2.dir/pic/ppm.c.o"
+  "CMakeFiles/CS420HW2.dir/pic/tiff.1.c.o"
+  "CMakeFiles/CS420HW2.dir/pic/tiff.c.o"
+  "CMakeFiles/CS420HW2.dir/pic/xpic.1.c.o"
+  "CMakeFiles/CS420HW2.dir/pic/xpic.2.c.o"
+  "CMakeFiles/CS420HW2.dir/pic/xpic.c.o"
+  "CS420HW2.pdb"
+  "CS420HW2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/CS420HW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
